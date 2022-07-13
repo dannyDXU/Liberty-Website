dbdd6549b71151b7b8a60c3481d92f142ed0a321
dannyDXU <85129208+dannyDXU@users.noreply.github.com>
2022-07-13 12:40:58 -0400

    Add files via upload

---
+++ 8.js
@@ -0,0 +1,9 @@
+var input = prompt("Would you like to learn some fun facts about CHINCHILLA? If yes, put 'yes'. If no, put 'no'");
+
+if (input == 'yes') {
+    alert("You answered " + input + ". Enjoy the funfacts about CHINCHILLA!");
+}
+
+else {
+    alert("You answered " + input + ". It seems you know all the funfacts about them! Please let us know too!!!");
+}
